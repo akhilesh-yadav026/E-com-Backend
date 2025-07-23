0e02576b719202951173616b521dd4f75bcd3471
akhilesh-yadav026 <akhilesh9426@gmail.com>
2025-07-23 21:07:56 +0530

    Add and Update

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import {addProduct , listproduct , removeproduct ,singleProduct} from '../controllers/productController.js'
+import upload from '../middleware/multer.js';
 
 const productRouter = express.Router();
 
-productRouter.post('/add' , addProduct)
+productRouter.post('/add' ,upload.fields([{name:'image1', maxCount : 1},{name:'image2', maxCount : 1},{name:'image3', maxCount : 1},{name:'image4', maxCount : 1}]), addProduct)
 productRouter.get("/list" , listproduct)
 productRouter.post('/remove', removeproduct)
 productRouter.post('/single', singleProduct)
 
 export default productRouter
+ 
